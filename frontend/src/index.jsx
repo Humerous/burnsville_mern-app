cd30585daf7d44adb772b5cf896ef8cfb238631d
David Miller <chameleonunicodestudios@gmail.com>
2026-08-29 21:20:21 +0200

    fix(mobile): load header QA corrections

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -5,6 +5,7 @@ import store from './store';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
+import './mobile-header-fixes.css';
 import * as serviceWorker from './serviceWorker';
 
 // <---- REDUX STORE ---->
